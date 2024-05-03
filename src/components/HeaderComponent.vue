edf805391f5a470fc74cf0e0db798e0e6a24b0ae
Kirill Kobunov <k.kobunov@intickets.ru>
2024-05-03 05:08:22 +0300

    feature container

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,15 +9,13 @@ const toggleMenu = () => {
   <div class="container-wide header">
     <div class="app-name">methodius weather app</div>
     <div class="menu-wrapper">
+      <input type="checkbox" id="check-menu">
       <nav id="nav">
-        <!--      <label for="check-menu"><span>Menu</span></label>-->
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
         <router-link to="/search">Search City</router-link>
       </nav>
-      <input type="checkbox" id="check-menu">
       <div class="toggle-nav" @click="toggleMenu">
-
         <div class="burger-line first"></div>
         <div class="burger-line second"></div>
         <div class="burger-line third"></div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container-wide">
     <div class="title-wrapper">
       <h1>
         Welcome to the weather app<br>
@@ -15,7 +15,10 @@
         <h2>Key features of My Weather App</h2>
       </div>
       <div class="feature-container">
-
+        <FeatureCard :title="data.features.first.title" :text="data.features.first.text"/>
+        <FeatureCard :title="data.features.second.title" :text="data.features.second.text"/>
+        <FeatureCard :title="data.features.third.title" :text="data.features.third.text"/>
+        <FeatureCard :title="data.features.fourth.title" :text="data.features.fourth.text"/>
       </div>
     </div>
 <!--    <div class="city-list">-->
@@ -27,6 +30,28 @@
 <script setup lang="ts">
 // const location = navigator.geolocation.getCurrentPosition()
 // import CityList from "@/components/CityList.vue";
+import FeatureCard from "@/components/FeatureCard.vue";
+
+const data = {
+  features: {
+    first: {
+      title: 'Current Weather',
+      text: "Get real-time weather information for your current location"
+    },
+    second: {
+      title: 'Three day forecast',
+      text: "Plan ahead with a detailed forecast for the next three days"
+    },
+    third: {
+      title: 'City Search',
+      text: "Easily search and view weather information for any city worldwide"
+    },
+    fourth: {
+      title: 'Customizable Settings',
+      text: "Personalize your weather app experience with customizable settings"
+    },
+  }
+}
 </script>
 
 <style lang="sass">
