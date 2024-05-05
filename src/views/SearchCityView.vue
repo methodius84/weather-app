23e343ba647450f0cef08d2efe4804a90b8472b0
Kirill Kobunov <wasd-sport@yandex.ru>
2024-05-05 05:07:59 +0300

    search city input style

--- src/views/SearchCityView.vue
+++ src/views/SearchCityView.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 
+import SearchIcon from "@/components/icons/SearchIcon.vue";
 </script>
 
 <template>
   <div class="container">
-    <h1>Search for the city</h1>
+    <div class="search-title">
+      <h1>Search for the city</h1>
+    </div>
+    <div class="search">
+
+      <div class="search__input">
+        <input type="text" name="input-text" id="input-text" required spellcheck="false">
+        <span class="placeholder">Type city name</span>
+      </div>
+      <SearchIcon/>
+    </div>
   </div>
 </template>
 
